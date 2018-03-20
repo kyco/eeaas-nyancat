2a24eedb22caf62ecb83db9dd2e0232e911777f2
Cornelius Weidmann <cornelius@responsive.co.za>
2018-03-20 19:44:47 +0200

    Remove dependency on jQuery.

--- nyancat.js
+++ nyancat.js
@@ -1,5 +1,4 @@
 import './nyancat.css';
-import $ from 'jquery';
 
 const NyanCat = {
   name: 'NyanCat',
@@ -12,12 +11,15 @@ const NyanCat = {
     // Config
     window.nyancat = window.nyancat || {};
     window.nyancat.active = true;
-    $('body').append(`
-      <div id="nyancat-wrapper">
-        <div class="nyan"></div>
-      </div>
-    `);
-    window.nyancat.markup = document.getElementById('nyancat-wrapper');
+    const wrapperId = `nyancat-wrapper-${new Date().getTime()}`;
+    window.nyancat.nyanClass = 'nyan';
+    window.nyancat.rainbowClass = 'rainbow';
+
+    const markup = document.createElement('div');
+    markup.id = wrapperId;
+    markup.innerHTML = `<div class="${window.nyancat.nyanClass}"></div>`;
+    document.body.appendChild(markup);
+    window.nyancat.markup = document.getElementById(wrapperId);
 
     let px = 0;
     let py = 0;
@@ -25,10 +27,10 @@ const NyanCat = {
     let posX = 100;
     let posY = 100;
     let rainbow = null;
-    const nyan = $('.nyan');
+    const nyan = document.querySelectorAll(`.${window.nyancat.nyanClass}`);
     const pilha = [];
     const altura = 800;
-    const largura = parseInt($('body').width(), 10);
+    const largura = parseInt(document.body.getBoundingClientRect().width, 10);
     const tamanhoTela = parseInt(largura / 9, 10);
 
     const getRandomInt = function(min, max) {
@@ -40,63 +42,61 @@ const NyanCat = {
       posY = e.pageY;
     };
 
-    $(document).on('mousemove', window.nyancat.mouseMove);
+    document.addEventListener('mousemove', window.nyancat.mouseMove);
 
     for (let i = 0; i < tamanhoTela; i++) {
-      const rain = $('<div class="rainbow"></div>').css('left', `${i * 9}px`);
-      $('#nyancat-wrapper').append(rain);
+      const rain = document.createElement('div');
+      rain.classList.add(window.nyancat.rainbowClass);
+      rain.style.left = `${i * 9}px`;
+      document.getElementById(wrapperId).appendChild(rain);
     }
-    rainbow = $('.rainbow');
+    rainbow = document.querySelectorAll(`.${window.nyancat.rainbowClass}`);
 
     const criarEstrela = function() {
       const rand = getRandomInt(3, 14);
       const tempoDeVida = getRandomInt(5, 10);
-      const star = $('<div class="star"></div>').css({
-        width: `${rand}px`,
-        height: `${rand}px`,
-        left: `${largura - 10}px`,
-        top: Math.floor(Math.random() * altura + 1),
-        transition: `all ${tempoDeVida}s linear`,
-        transform: 'translate(0px, 0px)'
-      });
-      $('#nyancat-wrapper').append(star);
-
-      window.setTimeout(() => {
-        star.css('transform', `translate(-${largura}px, 0px)`);
-      }, getRandomInt(5, 10) * 10);
-
-      window.setTimeout(() => {
-        star.remove();
-      }, tempoDeVida * 1000);
+      const star = document.createElement('div');
+      star.classList.add('star');
+      star.style.width = `${rand}px`;
+      star.style.height = `${rand}px`;
+      star.style.left = `${largura - 10}px`;
+      star.style.top = `${Math.floor(Math.random() * altura + 1)}px`;
+      star.style.transition = `all ${tempoDeVida}s linear`;
+      star.style.transform = 'translate(0px, 0px)';
+      document.getElementById(wrapperId).appendChild(star);
+      window.setTimeout(() => (star.style.transform = `translate(-${largura}px, 0px)`), getRandomInt(5, 10) * 10);
+      window.setTimeout(
+        () => document.getElementById(wrapperId) && document.getElementById(wrapperId).removeChild(star),
+        tempoDeVida * 1000
+      );
     };
 
     const moveNyan = function() {
-      const tamX = nyan.width() / 2;
-      const tamY = nyan.height() / 2;
+      const tamX = nyan[0].getBoundingClientRect().width / 2;
+      const tamY = nyan[0].getBoundingClientRect().height / 2;
       px += (posX - px - tamX) / 50;
       py += (posY - py - tamY) / 50;
-      nyan.css({
-        left: `${px}px`,
-        top: `${py}px`
-      });
+      nyan[0].style.left = `${px}px`;
+      nyan[0].style.top = `${py}px`;
     };
 
     const peidaArcoIris = function() {
-      const qnt = Math.floor(nyan.position().left / 9) + 2;
+      const qnt = Math.floor(nyan[0].offsetLeft / 9) + 2;
       if (pilha.length >= qnt) {
         pilha.pop();
       }
       pilha.unshift(py);
-      rainbow.hide();
+      rainbow.forEach(rainbow => (rainbow.style.display = 'none'));
       for (let i = 0; i < qnt; i++) {
         let am = i % 2;
         if (an) {
           am = i % 2 ? 0 : 1;
         }
-        rainbow
-          .eq(qnt - i)
-          .css('top', pilha[i] + am)
-          .show();
+        const currentRainbow = document.querySelectorAll(`.${window.nyancat.rainbowClass}`)[qnt - i];
+        if (currentRainbow) {
+          currentRainbow.style.top = `${pilha[i] + am}px`;
+          currentRainbow.style.display = 'block';
+        }
       }
     };
 
@@ -104,18 +104,12 @@ const NyanCat = {
       moveNyan();
       peidaArcoIris();
     }, 10);
-
-    window.nyancat.interval2 = window.setInterval(() => {
-      criarEstrela();
-    }, 300);
-
-    window.nyancat.interval3 = window.setInterval(() => {
-      an = !an;
-    }, 500);
+    window.nyancat.interval2 = window.setInterval(() => criarEstrela(), 300);
+    window.nyancat.interval3 = window.setInterval(() => (an = !an), 500);
 
     let frame = 0;
     window.nyancat.interval4 = window.setInterval(() => {
-      nyan.css('background-position', `${34 * frame}px`);
+      nyan[0].style.backgroundPosition = `${34 * frame}px`;
       frame++;
     }, 100);
   },
@@ -123,7 +117,7 @@ const NyanCat = {
   stop() {
     window.nyancat = window.nyancat || {};
     window.nyancat.active = false;
-    $(document).off('mousemove', window.nyancat.mouseMove);
+    document.removeEventListener('mousemove', window.nyancat.mouseMove);
     window.clearInterval(window.nyancat.interval1);
     window.clearInterval(window.nyancat.interval2);
     window.clearInterval(window.nyancat.interval3);
